c9d232394621f64f1e3e7d313e32a15377e1cdfa
cewerner <44435502+cewerner@users.noreply.github.com>
2018-11-18 13:22:44 +0100

    Galerie

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
+
+/*Slider - Start*/
 $('.autoplay').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
 });
+/*Slider - Ende*/
+
 /*Responsive Burgermenü - Start*/
 $(function() {
   var menuVisible = false;

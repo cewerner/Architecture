d2e3ae1254b935516066059451a908eb408ebe99
super-fancy <44967266+super-fancy@users.noreply.github.com>
2018-11-20 21:09:39 +0100

    wieder hergestellt

--- script.js
+++ script.js
@@ -154,9 +154,44 @@ $('figure').mouseout(function(){
 });*/
 /*Bildtexte bei Klick ein und ausblenden - Ende*/
 
-/*Scratchpad - Start*/
+/*Vorher-Nachher - Start*/
+var subject = document.querySelector('.before-and-after');
+var scraper = document.querySelector('.subject-scraper');
+var after = document.querySelector('.subject-after');
 
-/*Scratchpad - Ende*/
+var distance = (window.innerWidth - subject.clientWidth) / 2;
+window.onresize = recalculateDistance;
+
+var px = 0;
+var touches = [];
+
+subject.addEventListener('mousemove', dragScraper, false);
+subject.addEventListener('touchmove', dragScraper, false);
+
+function recalculateDistance() {
+
+  distance = (window.innerWidth - subject.clientWidth) / 2;
+  
+}
+
+function dragScraper(event) {
+  
+  px = event.clientX - distance;
+  
+  if(px == null) {
+    touches = event.touches;
+    px = touches[0].clientX - distance;
+  }
+  
+  if(px < 0) {
+    px = 0;
+  }
+  
+  scraper.style.transform = 'translate(' + px + 'px, 0)';
+  after.style.transform = 'translate(-' + px + 'px, 0)';
+  
+}
+/*Vorher-Nachher - Ende*/
 
 /*Formular auslesen - Start*/
 $('#send').click(function(){

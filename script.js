835138e84391962d28cac90804e99c64fe049266
cewerner <44435502+cewerner@users.noreply.github.com>
2018-11-21 13:31:53 +0100

    responsive Menü
    funktioniert bis auf erster Klick auf burger menü nach auswahl eines responsive menüpunktes - Doppelklick nötig

--- script.js
+++ script.js
@@ -110,57 +110,20 @@ $('body, html').css('overflow','visible');
           menuVisible = false;
   }
 });
-$(window).resize(function(){
-$('body,html').css('overflow','hidden');
-var screenWidth = $(window).width();
-$('body, html').css('overflow','visible');
-    if (screenWidth < 1080){
-    $('navbar ul li a').click(function(){
-      $('#Menü').css({'display':'none'});
-      menuVisible = false;
-      /*Smooth scrolling -Start*/
-
-      $('a').click(function(){
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-      && location.hostname == this.hostname) {
-          var $target = $(this.hash);
-          $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-          if ($target.length) {
-              var targetOffset = $target.offset().top;
-              var HeaderHeight = $("Header").outerHeight();
-              $('html,body,main').animate({scrollTop: targetOffset-HeaderHeight-20}, 700);
-              return false;
-          }
-      }
-      });
-      /*Smooth scrolling -Ende*/
-    })
-    }
-    else {$('navbar ul li a').click(function(){
-      $('#Menü').css({'display':'block'});
-      /*Smooth scrolling -Start*/
-
-      $('a').click(function(){
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-      && location.hostname == this.hostname) {
-          var $target = $(this.hash);
-          $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-          if ($target.length) {
-              var targetOffset = $target.offset().top;
-              var HeaderHeight = $("Header").outerHeight();
-              $('html,body,main').animate({scrollTop: targetOffset-HeaderHeight-20}, 700);
-              return false;
-          }
-      }
-      });
-      /*Smooth scrolling -Ende*/
-    })};
-  });
+$('navbar ul li a').click(function(){
+  var screenWidth = $(window).width();
+  if (screenWidth < 1080){
+    $('#Menü').css({'display':'none'});
+  }
+  else {
+    $('#Menü').css({'display':'block'});
+  }
+});
 /*Responsive Burgermenü - Ende*/
 
 /*Smooth scrolling -Start*/
 
-/*$('a').click(function(){
+$('a').click(function(){
 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
 && location.hostname == this.hostname) {
     var $target = $(this.hash);
@@ -172,7 +135,7 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
         return false;
     }
 }
-});*/
+});
 /*Smooth scrolling -Ende*/
 
 /*Hovereffekt Galerie - Start*/

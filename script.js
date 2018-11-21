7dd1a473adb087a02ebdda11c78a7eaaaf33f3c9
cewerner <44435502+cewerner@users.noreply.github.com>
2018-11-21 09:46:15 +0100

    Slashding

--- script.js
+++ script.js
@@ -155,7 +155,41 @@ $('figure').mouseout(function(){
 /*Bildtexte bei Klick ein und ausblenden - Ende*/
 
 /*Scratchpad - Start*/
+var subject = document.querySelector('.before-and-after');
+var scraper = document.querySelector('.subject-scraper');
+var after = document.querySelector('.subject-after');
 
+var distance = (window.innerWidth - subject.clientWidth) / 2;
+window.onresize = recalculateDistance;
+
+var px = 0;
+var touches = [];
+
+subject.addEventListener('mousemove', dragScraper, false);
+subject.addEventListener('touchmove', dragScraper, false);
+
+function recalculateDistance() {
+
+  distance = (window.innerWidth - subject.clientWidth) / 2;
+
+}
+
+function dragScraper(event) {
+
+  px = event.clientX - distance;
+
+  if(isNaN(px)) {
+    touches = event.touches;
+    px = touches[0].clientX - distance;
+  }
+  if(px < 0) {
+    px = 0;
+  }
+
+  scraper.style.transform = 'translate(' + px + 'px, 0)';
+  after.style.transform = 'translate(-' + px + 'px, 0)';
+
+};
 /*Scratchpad - Ende*/
 
 /*Formular auslesen - Start*/
